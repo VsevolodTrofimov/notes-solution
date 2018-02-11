d6e968a4217c2ca3f5ecce2909b13e834d06ae0b
v-trof <seva.trofimov2@gmail.com>
2018-02-11 17:31:12 +0300

    Implemented store

---
+++ src/store.js
@@ -0,0 +1,104 @@
+common.store = (function() {
+  const prossibleColors = ['blue', 'red', 'orange', 'yellow']
+  const note_backups = {}
+  const subscribers = []
+
+  const newNote = () => ({
+    title: '',
+    text: '',
+    color: undefined,
+    state: 'new'
+  })
+
+  const state = {
+    notes: {
+      '0': {
+        title: "Колок",
+        text: "Затащить на пятюню",
+        color: 'blue',
+        priority: true,
+      }, 
+      '1': {
+        color: 'orange',
+        title:"IMG",
+        img: "https://pp.userapi.com/c637917/v637917736/7213e/bKbdB4CLC5Q.jpg",
+        text:"OTHER TEXT LL",
+      },
+      'new': newNote()
+    }
+  }
+
+
+  const reactions = {
+    NOTE_ADD: () => {
+      const nextTmpId = 'tmp-' + String(Object.keys(state.notes).length - 2)
+      state.notes[nextTmpId] = state.notes['new']
+      state.notes[nextTmpId].state = 'default'
+      state.notes.new = newNote()
+    },
+
+    NOTE_EDIT: ({noteId}) => {
+      const note = state.notes[noteId]
+      note_backups[noteId] = Object.assign({}, note)
+      note.state = 'edit'
+    },
+    NOTE_EDIT_SAVE: ({noteId}) => {
+      const note = state.notes[noteId]
+      note.state = 'default'
+      delete note_backups[noteId]
+    },
+    NOTE_EDIT_CANCEL: ({noteId}) => {
+      const note = state.notes[noteId]
+      Object.assign(note, note_backups[noteId])
+      delete note_backups[noteId]
+    },
+    NOTE_TITLE_SET: ({noteId, value}) => {
+      state.notes[noteId].title = value
+    },
+    NOTE_TEXT_SET: ({noteId, value}) => {
+      state.notes[noteId].text = value
+    },
+
+    NOTE_COLOR_NEXT: ({noteId}) => {
+      const note = state.notes[noteId]
+      const idx = prossibleColors.indexOf(note.color)
+      note.color = prossibleColors[(idx + 1) % prossibleColors.length]
+    },
+    NOTE_PRIORITY_TOGGLE: ({noteId}) => {
+      state.notes[noteId].priority = !state.notes[noteId].priority
+    },
+
+    NOTE_DELETE: ({noteId}) => {
+      delete state.notes[noteId]
+    }
+  }
+
+  const react = action => {
+    if(reactions[action.type]) {
+      reactions[action.type](action)
+      subscribers.forEach(cb => cb(state))
+    }
+  }
+
+  const unsubscribe = cb => {
+    const idx = subscribers.indexOf(cb)
+    if(idx !== -1) subscribers.splice(idx, 1)
+  }
+
+  const subscribe = cb => {
+    subscribers.push(cb)
+    return () => unsubscribe(cb)
+  }
+
+  const dispatch = action => {
+    console.log(action)
+    react(action)
+  }
+
+  return {
+    state,
+    dispatch,
+    subscribe,
+    unsubscribe
+  }
+} ())
